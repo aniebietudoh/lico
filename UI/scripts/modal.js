8f53af5648dd7154ca0415ca25d9babd3137976a
ekikoh <faityfaity1@gmail.com>
2019-01-25 00:16:02 +0100

    Finished landing page, added new styles

--- UI/scripts/modal.js
+++ UI/scripts/modal.js
@@ -1,5 +1,5 @@
-let modal = document.getElementById('ui-modal');
-let btn = document.getElementById("open-modal");
+let modal = document.getElementById('ui-modal-register');
+let btn = document.getElementById("open-modal-register");
 let span = document.getElementsByClassName("close")[0];
 
 btn.onclick = () => {
@@ -10,8 +10,28 @@ span.onclick = () => {
   modal.style.display = "none";
 }
 
-window.onclick = (event) => {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+
+let modalLogin = document.getElementById('ui-modal-login');
+let btnLogin = document.getElementById("open-modal-login");
+let spanTwo = document.getElementsByClassName("close")[1];
+
+btnLogin.onclick = () => {
+  modalLogin.style.display = "block";
 }
+
+spanTwo.onclick = () => {
+  modalLogin.style.display = "none";
+}
+
+
+
+window.onclick = (event) => {
+  if (event.target == modalLogin) {
+    modalLogin.style.display = "none";
+  } else if (event.target == modal) {
+    modal.style.display = "none";
+  } 
+}
+
+
+
